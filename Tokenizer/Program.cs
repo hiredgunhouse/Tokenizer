6f3fb38a20ebc2562f92a6cd81c0305b12dae096
Piotr Owsiak <piotr.owsiak@o2.pl>
2015-01-12 22:49:47 +0100

    Initial version

---
+++ Tokenizer/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Tokenizer
+{
+    using System;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                PrintUsage();
+                return 0;
+            }
+
+            var tokenizer = new Tokenizer(args[0], args[1], args[2]);
+            return tokenizer.Run();
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\tTokenizer.exe template-file token-file output-file");
+        }
+    }
+}
