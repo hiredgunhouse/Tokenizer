e02718d6591143c15c46fe901e2f83be61192f00
Piotr Owsiak <Piotr.Owsiak@santanderconsumer.dk>
2017-06-12 12:26:43 +0200

    sln file version updated, ignores added, build & rebuild scripts added

---
+++ build.bat
@@ -0,0 +1,8 @@
+REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+SET SOLUTION_FILE=Tokenizer.sln
+
+%MSBUILD% %SOLUTION_FILE% /m %*
+
+pause
