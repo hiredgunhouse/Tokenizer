d77c371e4fb13457bb33661ea7e30f37c34245bb
Zbigniew Manasterski <zbigniew.manasterski@santanderconsumer.dk>
2017-12-12 15:03:44 +0100

    adding nuget restore build bat

--- rebuild.bat
+++ rebuild.bat
@@ -2,7 +2,8 @@ REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 SET SOLUTION_FILE=Tokenizer.sln
-
+rd /s /q packages
+nuget restore %SOLUTION_FILE%
 %MSBUILD% %SOLUTION_FILE% /m /t:Rebuild %*
 
 pause
